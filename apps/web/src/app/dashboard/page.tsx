e895438c061988d5e774eb85de0b90b6d824973b
n720-hd <devandjayakarta@Devans-MacBook-Pro.local>
2024-12-05 14:14:15 +0700

    feat: reinit same feature

---
+++ apps/web/src/app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+
+
+
+const DashboardPage = () => {
+
+    return (
+        <main className="gap-10 mx-auto py-28">
+            <div>
+                <div>
+                    <h1>HELLO TENANT</h1>
+                </div>
+                <h1>
+                    ACUMALAKA
+                </h1>
+            </div>
+        </main>
+    )
+
+} 
+
+export default DashboardPage
