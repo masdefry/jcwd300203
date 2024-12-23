cdacf593d74ada8e17b9164e0f8a87e6cfb03e65
Ideadly-Skies <Obie.kal22@gmail.com>
2024-12-23 13:31:12 +0700

    feat: implemented tenant transaction management system w/ fix

--- apps/api/src/routers/transaction.routers/index.ts
+++ apps/api/src/routers/transaction.routers/index.ts
@@ -9,12 +9,12 @@ import { verifyRoleTenant } from "@/middlewares/verify.role.tenant"
 const router = Router()
 
 // create a new room reservation
-router.post("/reserve",createRoomReservation)
+router.post("/reserve",verifyToken, verifyRoleCustomer, createRoomReservation)
 
 // upload payment proof route
 router.post("/upload-proof/:bookingId",verifyToken, verifyRoleCustomer, proofOfPaymentUploader, uploadPaymentProof)
 
 // tenant route
 router.post("/:bookingId/confirm",verifyToken, verifyRoleTenant, confirmPayment)
-    
+
 export default router
